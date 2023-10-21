d3703d999df918d503343c73247f9975565d2550
umarbinmusa <umarmusa20002@gmail.com>
2023-10-21 15:57:54 -0700

    adding resolver

---
+++ models/user.js
@@ -0,0 +1,14 @@
+
+[nodemon] 3.0.1
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,cjs,json
+[nodemon] starting `node server.js`
+Error connecting to the database: Error: querySrv ECONNREFUSED _mongodb._tcp.cluster0.uxuqarg.mongodb.net
+at QueryReqWrap.onresolve [as oncomplete] (node:internal/dns/promises:252:17) {
+errno: undefined, code: 'ECONNREFUSED',
+syscall: 'querySrv',
+hostname: '_mongodb._tcp.cluster0.uxuqarg.mongodb.net'
+}
+[nodemon] clean exit - waiting for changes before restart
+
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require("express")
-const { graphqlHTTP } = require("express-graphql")
-const { buildSchema } = require("graphql")
-const ConnectDB = require("./utils/connect")
-
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const ConnectDB = require("./utils/connect");
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
+`);
 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
-    return "Hello world!"
+    return "Hello world!";
   },
-}
+};
+
 const PORT = process.env.PORT || 5000;
+
 require("dotenv").config();
-const app = express()
+const app = express();
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -27,16 +29,17 @@ app.use(
     rootValue: root,
     graphiql: true,
   })
-)
+);
+
 const start = async () => {
   try {
     await ConnectDB(process.env.MONGODB_URI);
     app.listen(PORT, () => {
-      console.log(
-        `Connected to DB and Server is running on port ${PORT}`
-      );
+      console.log(`Connected to DB and Server is running on port ${PORT}`);
     });
   } catch (err) {
-    console.log(err);
+    console.error("Error connecting to the database:", err);
   }
 };
+
+start();
